2c90a070ac6cf37e4b81e51b8daf12c2d2f9bc11
raja <raja@raja.com>
2022-01-08 18:27:59 +0530

    all done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -54,7 +54,7 @@
 
                                 @if($about->title=="who-we-are")
                                 <p>
-                                    <?php echo substr($about->description,29,1000) ?>
+                                    <?php echo substr($about->description,30,9000) ?>
                                 </p>
 
                                 <!-- <img src="images/signature.png" alt="" class="mt-10 mb-15"> -->
