696960321770f7424d1f4e7b06c29eff1cca1fdf
raja <raja@raja.com>
2022-01-22 13:38:28 +0530

    all doen

--- resources/views/image-gallery.blade.php
+++ resources/views/image-gallery.blade.php
@@ -1,66 +1,68 @@
 <x-layout>
     @slot('title', 'About us')
-        @slot('body')
-            <!-- Start main-content -->
-            <div class="main-content">
+    @slot('body')
+        <!-- Start main-content -->
+        <div class="main-content">
 
-                <!-- Section: inner-header -->
-                <section class="inner-header divider layer-overlay overlay-theme-colored-7" data-bg-img="images/bg/bg1.jpg">
-                    <div class="container pt-120 pb-60">
-                        <!-- Section Content -->
-                        <div class="section-content">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <h2 class="text-theme-colored2 font-36">Image Gallery</h2>
-                                    <ol class="breadcrumb text-left mt-10 white">
-                                        <li><a href="#">Home</a></li>
-                                        <li class="active">Image Gallery</li>
-                                    </ol>
-                                </div>
+            <!-- Section: inner-header -->
+            <section class="inner-header divider layer-overlay overlay-theme-colored-7" data-bg-img="images/bg/bg1.jpg">
+                <div class="container pt-120 pb-60">
+                    <!-- Section Content -->
+                    <div class="section-content">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h2 class="text-theme-colored2 font-36">Image Gallery</h2>
+                                <ol class="breadcrumb text-left mt-10 white">
+                                    <li><a href="#">Home</a></li>
+                                    <li class="active">Image Gallery</li>
+                                </ol>
                             </div>
                         </div>
                     </div>
-                </section>
+                </div>
+            </section>
 
-                <!-- Gallery Grid 3 -->
-                <section>
-                    <div class="container">
-                        <div class="section-content">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <!-- Portfolio Filter -->
-                                    <div class="portfolio-filter">
-                                        <a href="#" class="active" data-filter="*">All</a>
-                                        <a href="#photo" class="" data-filter=".photo">Photo</a>
+            <!-- Gallery Grid 3 -->
+            <section>
+                <div class="container">
+                    <div class="section-content">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <!-- Portfolio Filter -->
+                                <div class="portfolio-filter">
+                                    <a href="#" class="active" data-filter="*">All</a>
+                                    <a href="#photo" class="" data-filter=".photo">Photo</a>
 
-                                        <a href="#video" class="" data-filter=".video">Video</a>
-                                    </div>
-                                    <!-- End Portfolio Filter -->
+                                    <a href="#video" class="" data-filter=".video">Video</a>
+                                </div>
+                                <!-- End Portfolio Filter -->
 
-                                    <!-- Portfolio Gallery Grid -->
-                                    <div id="grid" class="gallery-isotope default-animation-effect grid-4 gutter clearfix">
+                                <!-- Portfolio Gallery Grid -->
+                                <div id="grid" class="gallery-isotope default-animation-effect grid-4 gutter clearfix">
 
-                                        <!-- Portfolio Item Start -->
-                                        <div class="gallery-item video">
-                                            <div class="thumb">
-                                                <img class="img-fullwidth"  width="150px" height="180px" src="images/gallery/4.jpg" alt="project">
-                                                <div class="overlay-shade"></div>
-                                                <div class="icons-holder">
-                                                    <div class="icons-holder-inner">
-                                                        <div
-                                                            class="styled-icons icon-sm icon-dark icon-circled icon-theme-colored">
-                                                            <a class="popup-youtube"
-                                                                href="https://www.youtube.com/watch?v=C61pgS4W6X8"><i
-                                                                    class="fa fa-youtube-play"></i></a>
-                                                        </div>
+                                    <!-- Portfolio Item Start -->
+                                    <div class="gallery-item video">
+                                        <div class="thumb">
+                                            <img class="img-fullwidth" width="150px" height="180px"
+                                                src="images/gallery/4.jpg" alt="project">
+                                            <div class="overlay-shade"></div>
+                                            <div class="icons-holder">
+                                                <div class="icons-holder-inner">
+                                                    <div
+                                                        class="styled-icons icon-sm icon-dark icon-circled icon-theme-colored">
+                                                        <a class="popup-youtube"
+                                                            href="https://www.youtube.com/watch?v=C61pgS4W6X8"><i
+                                                                class="fa fa-youtube-play"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <!-- Portfolio Item End -->
+                                    </div>
+                                    <!-- Portfolio Item End -->
 
-                                        <!-- Portfolio Item Start -->
-                                        @foreach ($gallery_title as $title)
+                                    <!-- Portfolio Item Start -->
+                                    @foreach ($gallery_title as $title)
+                                        @if ($title->link != '')
                                             <div class="gallery-item photo">
                                                 <div class="thumb">
 
@@ -71,9 +73,10 @@
                                                                     ->where('title', $title->title)
                                                                     ->get(); ?>
                                                                 @foreach ($images as $image)
-                                                                    <li><a href="{{ $url .'gallery/'. $image->image_name }}"
-                                                                            title="{{ $image->title }}"><img width="150px" height="180px"
-                                                                                src="{{ $url .'gallery/'. $image->image_name }}"
+                                                                    <li><a href="{{ $url . 'gallery/' . $image->image_name }}"
+                                                                            title="{{ $image->title }}"><img width="150px"
+                                                                                height="180px"
+                                                                                src="{{ $url . 'gallery/' . $image->image_name }}"
                                                                                 alt=""></a></li>
                                                                 @endforeach
 
@@ -98,22 +101,23 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        @endforeach
+                                        @endif
+                                    @endforeach
 
-                                        <!-- Portfolio Item End -->
+                                    <!-- Portfolio Item End -->
 
 
 
-                                        <!-- Portfolio Item End -->
-                                    </div>
-                                    <!-- End Portfolio Gallery Grid -->
-
+                                    <!-- Portfolio Item End -->
                                 </div>
+                                <!-- End Portfolio Gallery Grid -->
+
                             </div>
                         </div>
                     </div>
-                </section>
+                </div>
+            </section>
 
-            </div>
-        @endslot
-    </x-layout>
+        </div>
+    @endslot
+</x-layout>
