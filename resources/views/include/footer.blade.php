32ff48a93bd5333e357cb4d2dd8b90815815e022
raja <raja@raja.com>
2022-02-01 19:14:58 +0530

    dsklfjdlskfj

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer id="footer" class="footer" data-bg-color="#212331">
-    <div class="container pt-70 pb-40">
+    <div class="container pt-70 ">
         <div class="row">
             <div class="col-sm-6 col-md-4">
                 <div class="widget dark">
@@ -9,21 +9,42 @@
                         <img src="{{ asset('images/flogo.png') }} " class="img-fluid mb-3"
                             alt="Netaji Subhas University" style="filter: drop-shadow(9px 9px 9px #000);">
                     </div>
+
                     <!-- <p>Netaji Subhas University is the first premier and one of the best University in Jharkhand, with a venerable legacy, highest academic standards, diverse educational programmes, distinguished faculty, illustrious alumni, varied co-curricular activities and modern infrastructure stands as the pride of Jharkhand and Eastern India. It is an unit of Sitwanto Devi Mahila Kalyan Sansthan.</p>                                    -->
+
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-1">
+                <div class="widget dark">
+                    <h4 class="widget-title line-bottom-theme-colored-2"> Links</h4>
+                    <ul class="angle-double-right list-border">
+                        <li><a href="{{ route('index') }}">Home</a></li>
+                        <li><a href="{{ route('about', 'who-we-are') }}">About Us</a></li>
+                        <li><a href="{{ route('image-gallery') }}">Gallery</a></li>
+                        <li><a href="{{ route('career') }}">Career</a></li>
+                        <li><a href="{{ route('contact') }}">Contact us</a></li>
+                        <li><a href="https://nsucms.in/nsucms/student/student_login">Login</a></li>
+                    </ul>
 
                 </div>
             </div>
             <div class="col-sm-6 col-md-2">
                 <div class="widget dark">
-                    <h4 class="widget-title line-bottom-theme-colored-2">Useful Links</h4>
+                    <?php $programs = DB::table('course_tbl')
+                        ->select('program')
+                        ->where('is_deleted', '1')
+                        ->groupBy('program')
+                        ->orderBy(DB::raw('count(id) '), 'DESC')
+                        ->limit(8)
+                        ->get(); ?>
+                    <h4 class="widget-title line-bottom-theme-colored-2">Courses</h4>
                     <ul class="angle-double-right list-border">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Gallery</a></li>
-                        <li><a href="#">Campus Life</a></li>
-                        <li><a href="#">Career</a></li>
-                        <li><a href="#">Contact us</a></li>
-                        <li><a href="#">Login</a></li>
+                        @foreach ($programs as $program)
+                            <li class=" text-capitalize"> <a
+                                    href="{{ route('program', strtolower(str_replace(' ', '-', $program->program))) }}"><b>
+                                        {{ ucfirst(strtolower($program->program)) }}
+                                </a> </li>
+                        @endforeach
                     </ul>
                 </div>
             </div>
@@ -31,45 +52,59 @@
                 <div class="widget dark">
                     <h4 class="widget-title line-bottom-theme-colored-2">Know More</h4>
                     <ul class="angle-double-right list-border">
-                        <li><a href="#">Offline Admission</a></li>
-                        <li><a href="#">Online Admission</a></li>
-                        <li><a href="#">Placement</a></li>
-                        <li><a href="#">List of Recruiters</a></li>
-                        <li><a href="#">Appointment Letter</a></li>
-                        <li><a href="#">Placement Contact</a></li>
-                        <li><a href="#">Download Brochure</a></li>
+                        <?php $infrastructures = DB::table('pages')
+                            ->where('page_type', 'infrastructure')
+                            ->limit(10)
+                            ->get();
+                        ?>
+                        @foreach ($infrastructures as $infrastructure)
+                            <li><a href="{{ route('infrastructure', $infrastructure->title) }}">
+                                    {{ $infrastructure->sub_title }}</a></li>
+                        @endforeach
                     </ul>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-4">
+            <div class="col-sm-6 col-md-3">
                 <div class="widget dark">
-                    <h4 class="widget-title line-bottom-theme-colored-2">Opening Hours</h4>
-                    <ul class="list-inline mt-5">
+                    <div class="widget dark">
+                        <ul class="angle-double-right list-border">
+                            <h4 class="widget-title line-bottom-theme-colored-2"> Education Loan</h4>
+                            <li><a href="https://propelld.com/signin">Propelld</a></li>
+                            <li><a href="https://neevfinance.hagnosoft.com/webform">neevfinance</a></li>
+                        </ul>
+                   <br>
 
-                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-map-marker text-theme-colored2 mr-5"></i> <a
-                                class="text-gray" href="#">Pokhari , Near Bhilai Pahadi ,
-                                Jamshedpur , Jharkhand </a> </li><br>
 
-                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-colored2 mr-5"></i> <a
-                                class="text-gray" href="#">0657-223-3022, 9386-817-857</a> </li><br>
-                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-theme-colored2 mr-5"></i> <a
-                                class="text-gray" href="#">info@nsuniv.ac.in</a> </li><br>
-                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-colored2 mr-5"></i> <a
-                                class="text-gray" href="#">https://nsuniv.ac.in/</a> </li>
-                    </ul>
-                    <ul class="styled-icons icon-sm icon-bordered icon-circled clearfix mt-10">
-                        <li><a target="_blank"
-                                href="https://api.whatsapp.com/send?phone=9386817857&text=&source=&data="><i
-                                    class="fa fa-whatsapp text-white"></i></a></li>
-                        <li><a target="_blank" href="https://www.facebook.com/nsu.jamshedpur/"><i
-                                    class="fa fa-facebook text-white"></i></a></li>
-                        <li><a target="_blank" href="https://www.youtube.com/channel/UCjndfC0cVjGnscin5RZgaFA"><i
-                                    class="fa fa-youtube text-white"></i></a></li>
-                        <li><a target="_blank" href="https://www.instagram.com/nsujamshedpur/?hl=en"><i
-                                    class="fa fa-instagram text-white"></i></a></li>
-                    </ul>
+                        <h4 class="widget-title line-bottom-theme-colored-2">Opening Hours</h4>
+                        <ul class="list-inline mt-5">
+
+                            <li class="m-0 pl-10 pr-10"> <i class="fa fa-map-marker text-theme-colored2 mr-5"></i> <a
+                                    class="text-gray" href="#">Pokhari , Near Bhilai Pahadi ,
+                                    Jamshedpur , Jharkhand </a> </li><br>
+
+                            <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-colored2 mr-5"></i> <a
+                                    class="text-gray" href="#">0657-223-3022, 9386-817-857</a> </li><br>
+                            <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-theme-colored2 mr-5"></i> <a
+                                    class="text-gray" href="#">info@nsuniv.ac.in</a> </li><br>
+                            <li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-colored2 mr-5"></i> <a
+                                    class="text-gray" href="#">https://nsuniv.ac.in/</a> </li>
+                        </ul>
+                        <ul class="styled-icons icon-sm icon-bordered icon-circled clearfix mt-10">
+                            <li><a target="_blank"
+                                    href="https://api.whatsapp.com/send?phone=9386817857&text=&source=&data="><i
+                                        class="fa fa-whatsapp text-white"></i></a></li>
+                            <li><a target="_blank" href="https://www.facebook.com/nsu.jamshedpur/"><i
+                                        class="fa fa-facebook text-white"></i></a></li>
+                            <li><a target="_blank" href="https://www.youtube.com/channel/UCjndfC0cVjGnscin5RZgaFA"><i
+                                        class="fa fa-youtube text-white"></i></a></li>
+                            <li><a target="_blank" href="https://www.instagram.com/nsujamshedpur/?hl=en"><i
+                                        class="fa fa-instagram text-white"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
+
             </div>
+
         </div>
     </div>
     <div class="footer-bottom" style="background: linear-gradient(
---
+++ resources/views/include/footer1.blade.php
@@ -0,0 +1,158 @@
+<!-- Footer -->
+<footer id="footer" class="footer" data-bg-color="#212331">
+    <div class="container pt-70 ">
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="widget dark">
+                    <div style="text-align:center;">
+                        <!-- <img class="mt-5 mb-20" alt="" src="images/flogo.png" style="width:150px;"> -->
+                        <img src="{{ asset('images/flogo.png') }} " class="img-fluid mb-3"
+                            alt="Netaji Subhas University" style="filter: drop-shadow(9px 9px 9px #000);">
+                    </div>
+                    <div class="widget dark">
+                        <br>
+                        <br>
+                        <h4 class="widget-title line-bottom-theme-colored-2">Opening Hours</h4>
+                        <ul class="list-inline mt-5">
+    
+                            <li class="m-0 pl-10 pr-10"> <i class="fa fa-map-marker text-theme-colored2 mr-5"></i> <a
+                                    class="text-gray" href="#">Pokhari , Near Bhilai Pahadi ,
+                                    Jamshedpur , Jharkhand </a> </li><br>
+    
+                            <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-colored2 mr-5"></i> <a
+                                    class="text-gray" href="#">0657-223-3022, 9386-817-857</a> </li><br>
+                            <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-theme-colored2 mr-5"></i> <a
+                                    class="text-gray" href="#">info@nsuniv.ac.in</a> </li><br>
+                            <li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-colored2 mr-5"></i> <a
+                                    class="text-gray" href="#">https://nsuniv.ac.in/</a> </li>
+                        </ul>
+                        <ul class="styled-icons icon-sm icon-bordered icon-circled clearfix mt-10">
+                            <li><a target="_blank"
+                                    href="https://api.whatsapp.com/send?phone=9386817857&text=&source=&data="><i
+                                        class="fa fa-whatsapp text-white"></i></a></li>
+                            <li><a target="_blank" href="https://www.facebook.com/nsu.jamshedpur/"><i
+                                        class="fa fa-facebook text-white"></i></a></li>
+                            <li><a target="_blank" href="https://www.youtube.com/channel/UCjndfC0cVjGnscin5RZgaFA"><i
+                                        class="fa fa-youtube text-white"></i></a></li>
+                            <li><a target="_blank" href="https://www.instagram.com/nsujamshedpur/?hl=en"><i
+                                        class="fa fa-instagram text-white"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- <p>Netaji Subhas University is the first premier and one of the best University in Jharkhand, with a venerable legacy, highest academic standards, diverse educational programmes, distinguished faculty, illustrious alumni, varied co-curricular activities and modern infrastructure stands as the pride of Jharkhand and Eastern India. It is an unit of Sitwanto Devi Mahila Kalyan Sansthan.</p>                                    -->
+
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-1">
+                <div class="widget dark">
+                    <h4 class="widget-title line-bottom-theme-colored-2"> Links</h4>
+                    <ul class="angle-double-right list-border">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Campus Life</a></li>
+                        <li><a href="#">Career</a></li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-2">
+                <div class="widget dark">
+                    <?php $programs = DB::table('course_tbl')
+                        ->select('program')
+                        ->where('is_deleted', '1')
+                        ->groupBy('program')
+                        ->orderBy(DB::raw('count(id) '), 'DESC')
+                        ->get(); ?>
+                    <h4 class="widget-title line-bottom-theme-colored-2">Courses</h4>
+                    <ul class="angle-double-right list-border">
+                        @foreach ($programs as $program)
+                            <li class=" text-capitalize"> <a
+                                    href="{{ route('program', strtolower(str_replace(' ', '-', $program->program))) }}"><b>
+                                       {{  ucfirst(strtolower($program->program)); }}
+                                </a> </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-2">
+                <div class="widget dark">
+                    <h4 class="widget-title line-bottom-theme-colored-2">Know More</h4>
+                    <ul class="angle-double-right list-border">
+                        <li><a href="#">Offline Admission</a></li>
+                        <li><a href="#">Online Admission</a></li>
+                        <li><a href="#">Placement</a></li>
+                        <li><a href="#">List of Recruiters</a></li>
+                        <li><a href="#">Appointment Letter</a></li>
+                        <li><a href="#">Placement Contact</a></li>
+                        <li><a href="#">Download Brochure</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <div class="widget dark">
+                    <h4 class="widget-title line-bottom-theme-colored-2">Opening Hours</h4>
+                    <ul class="list-inline mt-5">
+
+                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-map-marker text-theme-colored2 mr-5"></i> <a
+                                class="text-gray" href="#">Pokhari , Near Bhilai Pahadi ,
+                                Jamshedpur , Jharkhand </a> </li><br>
+
+                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-phone text-theme-colored2 mr-5"></i> <a
+                                class="text-gray" href="#">0657-223-3022, 9386-817-857</a> </li><br>
+                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-envelope-o text-theme-colored2 mr-5"></i> <a
+                                class="text-gray" href="#">info@nsuniv.ac.in</a> </li><br>
+                        <li class="m-0 pl-10 pr-10"> <i class="fa fa-globe text-theme-colored2 mr-5"></i> <a
+                                class="text-gray" href="#">https://nsuniv.ac.in/</a> </li>
+                    </ul>
+                    <ul class="styled-icons icon-sm icon-bordered icon-circled clearfix mt-10">
+                        <li><a target="_blank"
+                                href="https://api.whatsapp.com/send?phone=9386817857&text=&source=&data="><i
+                                    class="fa fa-whatsapp text-white"></i></a></li>
+                        <li><a target="_blank" href="https://www.facebook.com/nsu.jamshedpur/"><i
+                                    class="fa fa-facebook text-white"></i></a></li>
+                        <li><a target="_blank" href="https://www.youtube.com/channel/UCjndfC0cVjGnscin5RZgaFA"><i
+                                    class="fa fa-youtube text-white"></i></a></li>
+                        <li><a target="_blank" href="https://www.instagram.com/nsujamshedpur/?hl=en"><i
+                                    class="fa fa-instagram text-white"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom" style="background: linear-gradient(
+90deg, rgba(255,82,65,1) 0%, rgba(199,0,19,1) 100%)!important;">
+        <div class="container pt-20 pb-20">
+            <div class="row">
+                <div class="col-md-9">
+                    <p class="font-14 text-black-777 m-0 sm-text-center">
+                        &copy;
+                        <?php echo date('Y'); ?>
+                        Netaji Subhas University. All Rights Reserved || Developed With
+                        <i class="fa fa-heart" style="color:#c70013"></i> By
+                        <a href="http://infinitenetsolutions.com/" target="_blank">Infinite Net Solutions</a>
+                    </p>
+                </div>
+                <div class="col-md-3 text-right">
+                    <div class="widget no-border m-0">
+                        <ul class="list-inline sm-text-center mt-5 font-12">
+                            <li>
+                                <a href="#">FAQ</a>
+                            </li>
+                            <li>|</li>
+                            <li>
+                                <a href="#">Help Desk</a>
+                            </li>
+                            <li>|</li>
+                            <li>
+                                <a href="#">Support</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
